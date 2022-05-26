15923200140aee3b7d7fc4442ef38a7ccef2d134
ralepop <rale.popovic04@gmail.com>
2022-05-26 22:06:37 +0200

    email sender and login page without db

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, flash, redirect
-from forms import EmailForm
+from flask import Flask, render_template, flash, redirect, url_for
+from forms import EmailForm, LoginForm
 import smtplib
 from os import environ
+
 app = Flask(__name__)
+
+
+
 app.config['SECRET_KEY'] = 'todor-petkovic'
 app.config['SENSITIVE_PASSWORD'] = environ.get('SENSITIVE_PASSWORD')
 
@@ -22,8 +26,20 @@ def form():
         email = form.email.data
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
-        server.login('ovde mejl sa kod saljes', app.config['SENSITIVE_PASSWORD'])
-        server.sendmail('ovde mejl sa kod saljes', email, message)
-        return redirect('/')
+        server.login('ralespam04@gmail.com', app.config['SENSITIVE_PASSWORD'])
+        server.sendmail('ralespam04@gmail.com', email, message)
+        return redirect(url_for('index'))
 
-    return render_template('form.html', title='Email', form=form)
+    return render_template('form.html', title='Email', form=form, mail_sender='ralespam04@gmail.com')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if(form.validate_on_submit()):
+        flash(f'Korisničko ime: {form.username.data}')
+        flash(f'Šifra: {form.password.data}')
+        return redirect(url_for('index'))
+    
+    return render_template('login.html', title='Login', form=form)
+    
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField, TextAreaField
+from wtforms import StringField, SubmitField, EmailField, TextAreaField, PasswordField, BooleanField
 from wtforms.validators import DataRequired
 
 class EmailForm(FlaskForm):
@@ -7,3 +7,9 @@ class EmailForm(FlaskForm):
     email = EmailField('Email', validators=[DataRequired()])
     message = TextAreaField('Poruka', validators=[DataRequired()])
     submit = SubmitField('Posalji')
+
+class LoginForm(FlaskForm):
+    username = StringField('Korisničko ime', validators=[DataRequired()])
+    password = PasswordField('Šifra', validators=[DataRequired()])
+    remember_me = BooleanField('Zapamti me')
+    submit = SubmitField('Potvrdi')
